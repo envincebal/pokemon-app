c608766f7f89a114043ca0f44f6f356019549b5a
envincebal <vincentyan1@gmail.com>
2019-10-29 12:32:58 -0700

    Added IIFE around repository array and refactored forEach array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
-var repository = [{
-  name: "Charmander",
-  height: 0.6,
-  types: ["fire"]
-}, {
-  name: "Bulbasaur",
-  height: 0.7,
-  types: ["grass", "poison"]
-}, {
-  name: "Squirtle",
-  height: 0.5,
-  types: ["water"]
-}];
+var pokemonRepository = (function () {
+  var repository = [{
+    name: "Charmander",
+    height: 0.6,
+    types: ["fire"]
+  }, {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"]
+  }, {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["water"]
+  }];
 
-repository.forEach(pokemon => {
+  function add(item) {
+    return repository.push(item);
+  }
+
+  function getAll() {
+    return repository;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(pokemon => {
   if (pokemon.height > 0.6) {
     document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!</p>");
   } else {
