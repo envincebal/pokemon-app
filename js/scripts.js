7b87026f191e29ee901cacca2a11cb2963b9f18b
envincebal <vincentyan1@gmail.com>
2019-10-28 22:05:21 -0700

    Added for loop to iterate through Pokemon data.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,12 @@ var repository = [{
   height: 0.5,
   types: ["water"]
 }];
+
+for (let i = 0; i < repository.length; i++) {
+  var pokemon = repository[i];
+  if (pokemon.height > 0.6) {
+    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!</p>");
+  } else {
+    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>");
+  }
+}
