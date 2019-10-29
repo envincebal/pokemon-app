1de5b52936c60450a8a4feb78fe2352e097af286
envincebal <vincentyan1@gmail.com>
2019-10-29 12:23:54 -0700

    Added forEach iteration for repository array.

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,10 @@ var repository = [{
   types: ["water"]
 }];
 
-for (let i = 0; i < repository.length; i++) {
-  var pokemon = repository[i];
+repository.forEach(pokemon => {
   if (pokemon.height > 0.6) {
     document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!</p>");
   } else {
     document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>");
   }
-}
+});
